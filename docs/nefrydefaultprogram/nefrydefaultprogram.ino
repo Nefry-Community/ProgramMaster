66d534a58fcc37b6ebb30615130528eb057eb19d
wami <wamiwami@live.jp>
2017-01-08 15:51:12 +0900

    autoアップデート関連ファイルを追加

---
+++ docs/nefrydefaultprogram/nefrydefaultprogram.ino
@@ -0,0 +1,19 @@
+#include <Nefry.h>
+void setup() {
+  Nefry.setProgramName("DefaultProgram");
+  int i=Nefry.autoUpdate("/nefrydefaultprogram");
+  if(i==true){
+    Nefry.println("プログラムの更新はありません。");
+  }else{
+    Nefry.print("更新に問題が発生しました。");
+    Nefry.println("ネットに問題ないかチェック！");
+    Nefry.print("ErrCode : ");
+    Nefry.println(i);
+    Nefry.setLed(255,0,0);
+    //ErrCode
+  }
+}
+void loop() {
+Nefry.setLed(random(255),random(255),random(255));
+Nefry.ndelay(500);
+}
